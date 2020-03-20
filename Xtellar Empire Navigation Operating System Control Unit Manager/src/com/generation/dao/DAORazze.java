61e266ebaa49f754e67893a7ad4b8ef830768548
lorenzomasu <lorenzomasu@macbook-pro.lan>
2020-03-20 12:55:09 +0100

    Completata classe DAORazze

--- Xtellar Empire Navigation Operating System Control Unit Manager/src/com/generation/dao/DAORazze.java
+++ Xtellar Empire Navigation Operating System Control Unit Manager/src/com/generation/dao/DAORazze.java
@@ -2,46 +2,76 @@ package com.generation.dao;
 
 import java.math.BigInteger;
 import java.util.List;
+import java.util.Map;
 
 import com.generation.base.Entity;
+import com.generation.base.SmartList;
+import com.generation.db.IDatabase;
 import com.generation.entities.Razza;
 
-public class DAORazze implements IDAO, IDAORazze{
+public class DAORazze implements IDAO, IDAORazze {
+	
+	private IDatabase db; 
+	
+	public DAORazze(IDatabase db) {
+		this.db = db; 
+	}
 
 	@Override
 	public List<Razza> esiste(String caratteristica) {
-		// TODO Auto-generated method stub
+		String query = read.replace("tabella", "razze").replace("id = [id]", "descrizione = '" + caratteristica + "'"); 
 		return null;
 	}
 
 	@Override
 	public List<Entity> list() {
-		// TODO Auto-generated method stub
-		return null;
+		String query = read.replace("tabella","razze").replace("where id = [id]", "");
+		return list(query);
 	}
 
 	@Override
 	public List<Entity> list(String filtro) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Entity> ris = new SmartList<Entity>();
+		List<Map<String,String>> righe = db.rows(filtro);
+		for(Map<String,String> riga : righe)
+		{
+			Entity e = new Razza();
+			e.fromMap(riga);
+			ris.add(e);
+		}
+		
+		return ris;
 	}
 
 	@Override
 	public Entity load(BigInteger id) {
-		// TODO Auto-generated method stub
-		return null;
+		String query = read.replace("tabella","razze").replace("[id]",id+"");
+		Entity e = new Razza();
+		e.fromMap(db.row(query));
+		return e;
 	}
 
 	@Override
 	public Entity load(Entity e) {
-		// TODO Auto-generated method stub
-		return null;
+		Entity ris = null;
+		String query = "";
+		
+		if(e.getId() != null)
+			query = update.replace("tabella", "razze").replace("[id]",e.getId()+"");
+		else
+			query = insert.replace("tabella", "razze");
+		
+		Map<String, String> mappa = e.toMap();
+		BigInteger id = db.update(query,mappa);
+		ris = load(id);
+		return ris;
 	}
 
 	@Override
 	public boolean delete(BigInteger id) {
-		// TODO Auto-generated method stub
-		return false;
+		String query = delete.replace("tabella", "razze").replace("[id]",id+"");
+		BigInteger ris = db.update(query);
+		return ris == null; 
 	}
 
 }
