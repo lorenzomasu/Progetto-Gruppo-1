baf4b35f3fa85419b36093572042d5df06f8dfe8
Ylosp <Ylosp@192.168.178.20>
2020-03-20 14:27:17 +0100

    DAOEssere + Essere Entity

--- Xtellar Empire Navigation Operating System Control Unit Manager/src/com/generation/dao/DAOEsseri.java
+++ Xtellar Empire Navigation Operating System Control Unit Manager/src/com/generation/dao/DAOEsseri.java
@@ -2,63 +2,91 @@ package com.generation.dao;
 
 import java.math.BigInteger;
 import java.util.List;
+import java.util.Map;
 
 import com.generation.base.Entity;
+import com.generation.base.SmartList;
+import com.generation.db.IDatabase;
+import com.generation.entities.Pianeta;
 
 public class DAOEsseri implements IDAO, IDAOEsseri{
-
+	private IDatabase db;
+	private String nometabella = "esseri";
+	
+	
+	public DAOEsseri (IDatabase db) {
+		this.db = db;
+	}
+	
+	public List<Entity> list(String filtro)
+	{
+		List<Entity> ris = new SmartList<Entity>();
+		for(Map<String,String> riga : db.rows(filtro)) {
+			Entity e = new Pianeta();
+			if(e!=null) {
+				e.fromMap(riga);
+				ris.add(e);
+			}
+		}
+		return ris;
+	}
+	
 	@Override
 	public List<Entity> list() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Entity> list(String filtro) {
-		// TODO Auto-generated method stub
-		return null;
+		String query = read.replace("tabella", nometabella).replace("where id = ?;", "");
+		return list(query);
 	}
 
 	@Override
 	public Entity load(BigInteger id) {
-		// TODO Auto-generated method stub
-		return null;
+		Entity ris = new Pianeta();
+		ris.fromMap(db.row(read.replace("tabella",nometabella).replace("?", id+"")));
+		return ris;
 	}
 
 	@Override
 	public Entity load(Entity e) {
-		// TODO Auto-generated method stub
-		return null;
+		BigInteger id = null;
+		String query="";
+		if(e.getId()!=null) // la mia entity esiste gia nel db
+			query = update.replace("tabella", nometabella).replace("[id]", e.getId()+"");
+		else
+			query = insert.replace("tabella", nometabella);
+		id = db.update(query, e.toMap());
+		return load(id);
 	}
 
 	@Override
 	public boolean delete(BigInteger id) {
-		// TODO Auto-generated method stub
-		return false;
+		return db.update(delete.replace("tabella", nometabella).replace("[id]", id+"")) == null;
 	}
-
+	
+	
 	@Override
 	public double etaMedia() {
-		// TODO Auto-generated method stub
-		return 0;
+		String query = "select avg(anni) media from ("
+				+ "select (year(now()) - year(dob)) as anni from esseri) pippo;";
+		return Integer.parseInt(db.row(query).get("media"));
 	}
 
 	@Override
 	public int etaMassima() {
-		// TODO Auto-generated method stub
-		return 0;
+		String query = "select max(anni) massimo from ("
+				+ "select (year(now()) - year(dob)) as anni from esseri) pippo;";
+		return Integer.parseInt(db.row(query).get("massimo"));
 	}
 
 	@Override
 	public int etaMinima() {
-		// TODO Auto-generated method stub
-		return 0;
+		String query = "select min(anni) minimo from ("
+				+ "select (year(now()) - year(dob)) as anni from esseri) pippo;";
+		return Integer.parseInt(db.row(query).get("minimo"));
 	}
 
 	@Override
 	public int ripetizioneCognome(String cognome) {
-		// TODO Auto-generated method stub
-		return 0;
+		String query = "select count(*) conta from essere where cognome = '" + cognome + "'";
+		return Integer.parseInt(db.row(query).get("conta"));
 	}
 
 }
--- Xtellar Empire Navigation Operating System Control Unit Manager/src/com/generation/entities/Essere.java
+++ Xtellar Empire Navigation Operating System Control Unit Manager/src/com/generation/entities/Essere.java
@@ -1,6 +1,7 @@
 package com.generation.entities;
 
 import java.sql.Date;
+import java.util.Calendar;
 
 import com.generation.base.*;
 
@@ -8,4 +9,76 @@ public class Essere extends Entity implements IEssere{
 	private String nome, cognome;
 	private Date dob;
 	private int idrazza, idnumero, idpianeta;
+	public Essere(String nome, String cognome, Date dob, int idrazza, int idnumero, int idpianeta) {
+		super();
+		this.nome = nome;
+		this.cognome = cognome;
+		this.dob = dob;
+		this.idrazza = idrazza;
+		this.idnumero = idnumero;
+		this.idpianeta = idpianeta;
+	}
+	
+	public Essere() {}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public int getIdrazza() {
+		return idrazza;
+	}
+
+	public void setIdrazza(int idrazza) {
+		this.idrazza = idrazza;
+	}
+
+	public int getIdnumero() {
+		return idnumero;
+	}
+
+	public void setIdnumero(int idnumero) {
+		this.idnumero = idnumero;
+	}
+
+	public int getIdpianeta() {
+		return idpianeta;
+	}
+
+	public void setIdpianeta(int idpianeta) {
+		this.idpianeta = idpianeta;
+	}
+
+	@Override
+	public boolean isValid(String... params) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int eta() {
+		return Calendar.YEAR - Integer.parseInt(String.valueOf(dob).split("-")[0]);
+	}
+	
+	
 }
